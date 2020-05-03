bb5db360641240a6aecbd35239895e38b03d01fa
Faris Luthfan R <LRFaris24@gmail.com>
2020-05-03 08:08:21 +0700

    feat(component): add contact list component

--- src/components/contactList/List.jsx
+++ src/components/contactList/List.jsx
@@ -4,13 +4,31 @@ import { List as UIList, Image } from 'semantic-ui-react'
 export default class List extends Component {
   render() {
     return (
-      <UIList>
+      <UIList selection verticalAlign='middle'>
         <UIList.Item>
-          <Image avatar content='https://gravatar.com/avatar/2a69bcfdbba2571f04f43c8c3701dd9b?s=400&d=robohash&r=x' />
-          <UIList.Content>list 1</UIList.Content>
+          <Image avatar src='https://gravatar.com/avatar/42aee185213244597819df6dda92094c?s=400&d=mp&r=x' />
+          <UIList.Content>
+            <UIList.Description>Gianna</UIList.Description>
+          </UIList.Content>
         </UIList.Item>
-        <UIList.Item>list 2</UIList.Item>
-        <UIList.Item>list 3</UIList.Item>
+        <UIList.Item>
+          <Image avatar src='https://gravatar.com/avatar/42aee185213244597819df6dda92094c?s=400&d=mp&r=x' />
+          <UIList.Content>
+            <UIList.Description>Miller</UIList.Description>
+          </UIList.Content>
+        </UIList.Item>
+        <UIList.Item>
+          <Image avatar src='https://gravatar.com/avatar/42aee185213244597819df6dda92094c?s=400&d=mp&r=x' />
+          <UIList.Content>
+            <UIList.Description>Dawes</UIList.Description>
+          </UIList.Content>
+        </UIList.Item>
+        <UIList.Item>
+          <Image avatar src='https://gravatar.com/avatar/42aee185213244597819df6dda92094c?s=400&d=mp&r=x' />
+          <UIList.Content>
+            <UIList.Description>Hanna</UIList.Description>
+          </UIList.Content>
+        </UIList.Item>
       </UIList>
     )
   }
--- src/pages/ContactList.js
+++ src/pages/ContactList.js
@@ -1,16 +1,43 @@
 import React from 'react'
 import Header from '../components/Header'
 import List from '../components/contactList/List'
-import { Container } from 'semantic-ui-react'
+import { css } from '@emotion/core'
+import { Menu } from 'semantic-ui-react'
+
+const listStyle = css`
+  .ui.list > .item > .content {
+    padding-left: 36px;
+  }
+
+  .ui.list > .item {
+    padding-top: 0px;
+    padding-left: 15px;
+    padding-right: 15px;
+    padding-bottom: 0px;
+  }
+
+  .ui.avatar.image {
+    width: 3.6em;
+    height: 3.6em;
+    margin-top: 9px;
+    margin-bottom: 9px;
+    margin-left: 13px;
+  }
+`
 
 export default class ContactList extends React.Component {
   render() {
     return (
       <div>
-        <Header />
-        <Container>
+        <Header>
+          <Menu>
+            <Menu.Item icon='angle left'></Menu.Item>
+            <Menu.Item content='Select User' />
+          </Menu>
+        </Header>
+        <div css={listStyle}>
           <List />
-        </Container>
+        </div>
       </div>
     )
   }
