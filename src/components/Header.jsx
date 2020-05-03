8de8c7e5182972b89121663bb0aa45ec31cbc72e
Faris Luthfan R <LRFaris24@gmail.com>
2020-05-03 08:06:51 +0700

    feat(header): add header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,7 @@
 import React from 'react'
-import { Menu } from 'semantic-ui-react'
 
 export default class Header extends React.Component {
   render() {
-    return (
-      <nav>
-        <Menu>
-          <Menu.Item icon='angle left'></Menu.Item>
-          <Menu.Item content='Select User' />
-        </Menu>
-      </nav>
-    )
+    return <nav>{this.props.children}</nav>
   }
 }
