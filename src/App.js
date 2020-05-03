4241c1dc678315b84ea473b94f59095830600c6b
Faris Luthfan R <LRFaris24@gmail.com>
2020-05-03 08:07:23 +0700

    feat(component): add chat component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
 import Login from './pages/Login'
 import ContactList from './pages/ContactList'
+import Chat from './pages/Chat'
 
 function App() {
   return (
     <Switch>
       <Route path='/login' component={Login} />
       <Route path='/contact-list' component={ContactList} />
+      <Route path='/chat' component={Chat} />
       <Route exact path='/'>
         <Redirect to='/login' />
       </Route>
